80f18e2d18dfe2a305c6bf227544206e58a4426f
NetoTTT <142108410+NetoTTT@users.noreply.github.com>
2024-02-28 16:40:01 -0300

    Create valida.js

---
+++ Sistema de Login/js/valida.js
@@ -0,0 +1,14 @@
+function validar_campos_login(){
+    var email = formlogin.email;
+    var senha = formlogin.senha;
+
+    if(email.value == ""){
+        alert("E-mail não fornecido");
+        email.focus();
+    }else if(senha.value == ""){
+        alert("Senha não fornecida");
+        senha.focus();
+
+    }
+
+}
